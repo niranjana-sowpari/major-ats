21d69a87ba78402417e97af4df0a23279e6d282c
niranjana-sowpari <sowpari@gmail.com>
2022-06-09 18:34:31 +0530

    changes

--- main/resume_parser.py
+++ main/resume_parser.py
@@ -29,7 +29,8 @@ class class_res:
 
 		#nltk.download('stopwords')  ### run this on python shell
 
-		x = "C:/Users/RIMA/Desktop/Django/mysite"
+		#x = "C:/Users/RIMA/Desktop/Django/mysite"
+		x = "local path"
 		
 		#value = str(settings.BASE_DIR)
 		#x = value + path
